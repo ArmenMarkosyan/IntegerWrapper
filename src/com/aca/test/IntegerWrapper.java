68e42827b2fbe5b7b32f97cb641e90120a1e8234
unknown <armen.markosyan@gmail.com>
2019-04-13 18:59:54 +0400

    add IntegerWrapper class

--- src/com/aca/test/IntegerWrapper.java
+++ src/com/aca/test/IntegerWrapper.java
@@ -1,4 +1,19 @@
 package com.aca.test;
 
 public class IntegerWrapper {
+    private int number;
+
+    public IntegerWrapper(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public static void swap(IntegerWrapper i1, IntegerWrapper i2) {
+        int a = i1.number;
+        i1.number = i2.number;
+        i2.number = a;
+    }
 }
--- src/com/aca/test/test.java
+++ src/com/aca/test/test.java
@@ -3,5 +3,12 @@ package com.aca.test;
 public class test {
     public static void main(String[] args) {
 
+        IntegerWrapper integerWrapper1 = new IntegerWrapper(5);
+        IntegerWrapper integerWrapper2 = new IntegerWrapper(10);
+
+        IntegerWrapper.swap(integerWrapper1, integerWrapper2);
+
+        System.out.println("integerWrapper1 : " + integerWrapper1.getNumber());
+        System.out.println("integerWrapper2 : " + integerWrapper2.getNumber());
     }
 }
